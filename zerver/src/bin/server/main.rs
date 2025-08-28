260c0ea50593fa4daec497976762c9e1dc536cda
scadoshi <scottyfermo@hotmail.com>
2025-08-27 22:20:18 -0700

    Updated main to support card service

--- zerver/src/bin/server/main.rs
+++ zerver/src/bin/server/main.rs
@@ -1,6 +1,6 @@
 // internal
 use zwipe::config::Config;
-use zwipe::domain::{auth, health, logo, user};
+use zwipe::domain::{auth, card, health, logo, user};
 use zwipe::inbound::http::{HttpServer, HttpServerConfig};
 use zwipe::outbound::sqlx::postgres::Postgres;
 // external
@@ -24,11 +24,18 @@ async fn run() -> anyhow::Result<()> {
     let auth_service = auth::services::Service::new(db.clone(), config.jwt_secret);
     let user_service = user::services::Service::new(db.clone());
     let health_service = health::services::Service::new(db.clone());
+    let card_service = card::services::Service::new(db.clone());
     let server_config = HttpServerConfig {
         bind_address: &config.bind_address,
         allowed_origins: config.allowed_origins,
     };
-    let http_server =
-        HttpServer::new(auth_service, user_service, health_service, server_config).await?;
+    let http_server = HttpServer::new(
+        auth_service,
+        user_service,
+        health_service,
+        card_service,
+        server_config,
+    )
+    .await?;
     http_server.run().await
 }
