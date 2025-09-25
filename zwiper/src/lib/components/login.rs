aa43a168663e528aa16d5114423544697aa9c463
scadoshi <scottyfermo@hotmail.com>
2025-09-24 19:59:16 -0700

    added register button

--- zwiper/src/lib/components/login.rs
+++ zwiper/src/lib/components/login.rs
@@ -2,59 +2,62 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Login() -> Element {
-    // These are our reactive state variables
     let mut username_or_email = use_signal(|| "".to_string());
     let mut password = use_signal(|| "".to_string());
 
-    // Handler for form submission
-    let handle_submit = move |event: Event<FormData>| {
-        event.prevent_default(); // Prevent page refresh
-        println!("login attempt:");
-        println!("  identity: {}", username_or_email.read());
-        println!("  password: {}", password.read());
-        // TODO: Call your backend API here
-    };
-
     rsx! {
-        div { class: "login-container",
+        div { class : "login-container",
             h2 { "login" }
 
             form {
-                onsubmit: handle_submit,
+                onsubmit : move |_| {
+                    println!("need to build this still");
+                },
 
-                div { class: "form-group",
+                div {
+                    class : "form-group",
                     label { r#for: "identity", "username or email" }
                     input {
-                        id: "identity",
-                        r#type: "text",
-                        placeholder: "",
-                        value: "{username_or_email}",
-                        autocapitalize: "none",
-                        spellcheck: "false",
+                        id : "identity",
+                        r#type : "text",
+                        placeholder : "",
+                        value : "{username_or_email}",
+                        autocapitalize : "none",
+                        spellcheck : "false",
                         oninput: move |event| {
                             username_or_email.set(event.value());
                         }
                     }
                 }
 
-                div { class: "form-group",
-                    label { r#for: "password", "password" }
+                div {
+                    class : "form-group",
+                    label { r#for : "password", "password" }
                     input {
-                        id: "password",
-                        r#type: "password",
-                        placeholder: "",
-                        value: "{password}",
-                        autocapitalize: "none",
-                        spellcheck: "false",
-                        oninput: move |event| {
+                        id : "password",
+                        r#type : "password",
+                        placeholder : "",
+                        value : "{password}",
+                        autocapitalize : "none",
+                        spellcheck : "false",
+                        oninput : move |event| {
                             password.set(event.value());
                         }
                     }
                 }
 
                 button {
-                    r#type: "submit",
+                    r#type : "submit",
                     "login"
+                }
+
+                button {
+                    onclick : move |_| {
+                        println!("need to build this still");
+                    },
+                    r#type : "submit",
+                    class : "register",
+                    "create profile"
                 }
             }
         }
