29acabd025ea74f5fe8d2ccf51f1197bed345a34
scadoshi <scottyfermo@hotmail.com>
2025-10-02 10:04:45 -0700

    navigation ability for clicking while working on this while not in iOS that allows swiping so we can still get around while developing

--- zwiper/src/lib/components/layout.rs
+++ zwiper/src/lib/components/layout.rs
@@ -4,11 +4,16 @@ use zwipe::domain::ascii_logo;
 
 #[component]
 pub fn Layout() -> Element {
+    let navigator = use_navigator();
     let ascii_logo = ascii_logo::logo();
 
     rsx! {
         div { class: "app-layout",
-            header { class: "app-header",
+            header {
+                onclick : move |_| {
+                    navigator.push(Route::Home {});
+                },
+                class: "app-header",
                 pre { class: "ascii-logo", "{ascii_logo}" }
             }
             main { class: "app-content",
--- zwiper/src/lib/routing/home.rs
+++ zwiper/src/lib/routing/home.rs
@@ -1,21 +1,33 @@
 use dioxus::prelude::*;
 
+use crate::routing::Route;
+
 #[component]
 pub fn Home() -> Element {
+    let navigator = use_navigator();
     rsx! {
+
         div { class : "home-directions",
-            div { class : "home-direction-arrow",
-            "🡅"
+            div {
+                onclick : move |_| {
+                    navigator.push(Route::Login {});
+                },
+                class : "home-direction-arrow",
+                "🡅"
             }
             p {
                 "swipe ", b { "up" }, " to ", b { "log in" }
             },
-            br {}, br {}, br {}
+            br {}, br {}, br {}, br {}
             p {
                 "swipe ", b { "down" }, " to ", b { "create profile" }
             },
-            div { class : "home-direction-arrow",
-            "🡇"
+            div {
+                onclick : move |_| {
+                    navigator.push(Route::Register {});
+                },
+                class : "home-direction-arrow",
+                "🡇"
             }
         }
     }
