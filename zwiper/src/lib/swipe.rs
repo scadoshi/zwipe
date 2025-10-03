0fde7f2b7d80f8874cbf2c8b39d981569792dc3f
scadoshi <scottyfermo@hotmail.com>
2025-10-03 16:33:18 -0700

    Organize swipe functions into State impl and trait-based event handlers

--- zwiper/src/lib/swipe.rs
+++ zwiper/src/lib/swipe.rs
@@ -1,10 +1,11 @@
+use chrono::NaiveDateTime;
 use dioxus::{
     html::{geometry::ClientPoint, input_data::MouseButton},
     prelude::*,
 };
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Place {
+pub enum Screen {
     Home,
     Login,
     Register,
@@ -12,105 +13,135 @@ pub enum Place {
 
 #[derive(Debug, Clone)]
 pub struct State {
-    pub place: Place,
+    pub screen: Screen,
     pub start: Option<ClientPoint>,
+    pub previous: Option<ClientPoint>,
     pub current: Option<ClientPoint>,
-    pub dx: f64,
-    pub dy: f64,
-    pub moving: bool,
+    pub timestamp: Option<NaiveDateTime>,
+    pub transition_seconds: f64,
 }
 
 impl State {
     pub fn new() -> Self {
         Self {
-            place: Place::Home,
+            screen: Screen::Home,
             start: None,
+            previous: None,
             current: None,
-            dx: 0.0,
-            dy: 0.0,
-            moving: false,
+            timestamp: None,
+            transition_seconds: 0.0,
         }
     }
 
     pub fn reset(&mut self) {
-        self.moving = false;
         self.start = None;
+        self.previous = None;
         self.current = None;
-        self.dx = 0.0;
-        self.dy = 0.0;
+        self.timestamp = None;
     }
-}
 
-pub fn handle_ontouchstart(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
-    if let Some(t) = e.touches().into_iter().next() {
-        swipe_state.with_mut(|ss| {
-            ss.start = Some(t.client_coordinates());
-            ss.current = ss.start.clone();
-        });
-
-        println!("touch start={:?}", swipe_state.read().start);
-    }
-}
-
-pub fn handle_ontouchmove(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
-    if let Some(t) = e.touches().into_iter().next() {
-        swipe_state.with_mut(|ss| {
-            ss.moving = true;
-            ss.current = Some(t.client_coordinates());
-            if let Some(start) = ss.start {
-                if let Some(current) = ss.current {
-                    ss.dx = start.x - current.x;
-                    ss.dy = start.y - current.y;
-                }
-            }
-        });
-    }
-}
-
-pub fn handle_ontouchend(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
-    swipe_state.with_mut(|ss| {
-        ss.moving = false;
-        if let Some(t) = e.touches().into_iter().next() {
-            ss.current = Some(t.client_coordinates());
+    pub fn dx(&self) -> f64 {
+        if let (Some(start), Some(current)) = (self.start, self.current) {
+            return start.x - current.x;
         }
-    });
-    let ss = swipe_state.clone();
-    println!("touch end={:?}", ss.read().current);
+        return 0.0;
+    }
 
-    swipe_state.with_mut(|ss| ss.reset());
-}
+    pub fn dy(&self) -> f64 {
+        if let (Some(start), Some(current)) = (self.start, self.current) {
+            return start.y - current.y;
+        }
+        return 0.0;
+    }
 
-pub fn handle_onmousedown(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
-    swipe_state.with_mut(|ss| {
-        ss.start = Some(e.client_coordinates());
-        ss.current = ss.start.clone();
-    });
+    pub fn speedy(&self) -> f64 {
+        let now = chrono::Utc::now().naive_utc();
+        todo!()
+    }
 
-    println!("mouse down={:?}", swipe_state.read().start);
-}
-
-pub fn handle_onmousemove(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
-    if e.held_buttons().contains(MouseButton::Primary) {
-        swipe_state.with_mut(|ss| {
-            ss.moving = true;
-            ss.current = Some(e.client_coordinates());
-            if let Some(start) = ss.start {
-                if let Some(current) = ss.current {
-                    ss.dx = start.x - current.x;
-                    ss.dy = start.y - current.y;
-                }
-            }
-        });
+    pub fn speedx(&self) -> f64 {
+        let now = chrono::Utc::now().naive_utc();
+        todo!()
     }
 }
 
-pub fn handle_onmouseup(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
-    swipe_state.with_mut(|ss| {
-        ss.moving = false;
-        ss.current = Some(e.client_coordinates());
-    });
-    let ss = swipe_state.clone();
-    println!("mouse up={:?}", ss.read().current);
+pub trait OnTouch {
+    fn handle_ontouchstart(&mut self, e: Event<TouchData>);
+    fn handle_ontouchmove(&mut self, e: Event<TouchData>);
+    fn handle_ontouchend(&mut self, e: Event<TouchData>);
+}
 
-    swipe_state.with_mut(|ss| ss.reset());
+impl OnTouch for Signal<State> {
+    fn handle_ontouchstart(&mut self, e: Event<TouchData>) {
+        if let Some(t) = e.touches().into_iter().next() {
+            self.with_mut(|ss| {
+                ss.transition_seconds = 0.0;
+
+                ss.start = Some(t.client_coordinates());
+                ss.current = ss.start.clone();
+            });
+
+            println!("touch start={:?}", self.read().start);
+        }
+    }
+
+    fn handle_ontouchmove(&mut self, e: Event<TouchData>) {
+        if let Some(t) = e.touches().into_iter().next() {
+            self.with_mut(|ss| {
+                ss.current = Some(t.client_coordinates());
+            });
+        }
+    }
+
+    fn handle_ontouchend(&mut self, e: Event<TouchData>) {
+        self.with_mut(|ss| {
+            ss.transition_seconds = 0.5;
+            if let Some(t) = e.touches().into_iter().next() {
+                ss.current = Some(t.client_coordinates());
+            }
+        });
+        let ss = self.clone();
+        println!("touch end={:?}", ss.read().current);
+
+        self.with_mut(|ss| ss.reset());
+    }
+}
+
+pub trait OnMouse {
+    fn handle_onmousedown(&mut self, e: Event<MouseData>);
+    fn handle_onmousemove(&mut self, e: Event<MouseData>);
+    fn handle_onmouseup(&mut self, e: Event<MouseData>);
+}
+
+impl OnMouse for Signal<State> {
+    fn handle_onmousedown(&mut self, e: Event<MouseData>) {
+        self.with_mut(|ss| {
+            ss.transition_seconds = 0.0;
+
+            let some_point = Some(e.client_coordinates());
+            ss.start = some_point.clone();
+            ss.current = some_point.clone();
+            ss.previous = some_point.clone();
+        });
+
+        println!("touch start={:?}", self.read().start);
+    }
+
+    fn handle_onmousemove(&mut self, e: Event<MouseData>) {
+        if e.held_buttons().contains(MouseButton::Primary) {
+            self.with_mut(|ss| {
+                ss.current = Some(e.client_coordinates());
+            });
+        }
+    }
+
+    fn handle_onmouseup(&mut self, e: Event<MouseData>) {
+        self.with_mut(|ss| {
+            ss.current = Some(e.client_coordinates());
+        });
+        let ss = self.clone();
+        println!("touch end={:?}", ss.read().current);
+
+        self.with_mut(|ss| ss.reset());
+    }
 }
