f95e4765a9f38e78ac515b7b5d32695cc624cd3f
scadoshi <scottyfermo@hotmail.com>
2025-10-03 13:30:48 -0700

    Consolidate touch and mouse event handlers into reusable functions with state management

--- zwiper/src/lib/swipe.rs
+++ zwiper/src/lib/swipe.rs
@@ -1,4 +1,7 @@
-use dioxus::html::geometry::ClientPoint;
+use dioxus::{
+    html::{geometry::ClientPoint, input_data::MouseButton},
+    prelude::*,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Place {
@@ -36,4 +39,78 @@ impl State {
         self.dx = 0.0;
         self.dy = 0.0;
     }
+}
+
+pub fn handle_ontouchstart(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
+    if let Some(t) = e.touches().into_iter().next() {
+        swipe_state.with_mut(|ss| {
+            ss.start = Some(t.client_coordinates());
+            ss.current = ss.start.clone();
+        });
+
+        println!("touch start={:?}", swipe_state.read().start);
+    }
+}
+
+pub fn handle_ontouchmove(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
+    if let Some(t) = e.touches().into_iter().next() {
+        swipe_state.with_mut(|ss| {
+            ss.moving = true;
+            ss.current = Some(t.client_coordinates());
+            if let Some(start) = ss.start {
+                if let Some(current) = ss.current {
+                    ss.dx = start.x - current.x;
+                    ss.dy = start.y - current.y;
+                }
+            }
+        });
+    }
+}
+
+pub fn handle_ontouchend(e: Event<TouchData>, swipe_state: &mut Signal<State>) {
+    swipe_state.with_mut(|ss| {
+        ss.moving = false;
+        if let Some(t) = e.touches().into_iter().next() {
+            ss.current = Some(t.client_coordinates());
+        }
+    });
+    let ss = swipe_state.clone();
+    println!("touch end={:?}", ss.read().current);
+
+    swipe_state.with_mut(|ss| ss.reset());
+}
+
+pub fn handle_onmousedown(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
+    swipe_state.with_mut(|ss| {
+        ss.start = Some(e.client_coordinates());
+        ss.current = ss.start.clone();
+    });
+
+    println!("mouse down={:?}", swipe_state.read().start);
+}
+
+pub fn handle_onmousemove(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
+    if e.held_buttons().contains(MouseButton::Primary) {
+        swipe_state.with_mut(|ss| {
+            ss.moving = true;
+            ss.current = Some(e.client_coordinates());
+            if let Some(start) = ss.start {
+                if let Some(current) = ss.current {
+                    ss.dx = start.x - current.x;
+                    ss.dy = start.y - current.y;
+                }
+            }
+        });
+    }
+}
+
+pub fn handle_onmouseup(e: Event<MouseData>, swipe_state: &mut Signal<State>) {
+    swipe_state.with_mut(|ss| {
+        ss.moving = false;
+        ss.current = Some(e.client_coordinates());
+    });
+    let ss = swipe_state.clone();
+    println!("mouse up={:?}", ss.read().current);
+
+    swipe_state.with_mut(|ss| ss.reset());
 }
