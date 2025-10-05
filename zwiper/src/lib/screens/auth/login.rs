083f9c21df0ea944343cddbb50925b2afa3d95e0
scadoshi <scottyfermo@hotmail.com>
2025-10-04 23:16:49 -0700

    add swipe-to-submit detection with extracted validation closure

--- zwiper/src/lib/screens/auth/login.rs
+++ zwiper/src/lib/screens/auth/login.rs
@@ -11,6 +11,28 @@ pub fn Login(swipe_state: Signal<swipe::State>) -> Element {
     let mut password = use_signal(|| String::new());
     let mut invalid_credentials = use_signal(|| false);
 
+    let mut attempt_submit = move || {
+        let valid_identifier = match (
+            Username::new(&username_or_email.read()),
+            EmailAddress::from_str(&username_or_email.read()),
+        ) {
+            (Ok(_), _) | (_, Ok(_)) => true,
+            (Err(_), Err(_)) => false,
+        };
+
+        let valid_password = Password::new(&password.read()).is_ok();
+
+        if !valid_identifier || !valid_password {
+            invalid_credentials.set(true);
+            return;
+        }
+
+        invalid_credentials.set(false);
+        println!("please log me in");
+    };
+
+    const ALLOWED_DIRECTIONS: [Dir; 1] = [Dir::Up];
+
     rsx! {
         div { class : "swipe-able",
 
@@ -26,16 +48,24 @@ pub fn Login(swipe_state: Signal<swipe::State>) -> Element {
             ontouchstart : move |e: Event<TouchData>| swipe_state.ontouchstart(e),
             ontouchmove : move |e: Event<TouchData>| swipe_state.ontouchmove(e),
             ontouchend : move |e: Event<TouchData>| {
-                swipe_state.ontouchend(e, &[Dir::Up, Dir::Down]);
-                println!("direction => {:?}", swipe_state.read().previous_swipe);
+                swipe_state.ontouchend(e, &ALLOWED_DIRECTIONS);
+                if swipe_state.read().previous_swipe == Some(Dir::Down) {
+                    attempt_submit();
+                }
             },
 
             onmousedown : move |e: Event<MouseData>| swipe_state.onmousedown(e),
             onmousemove : move |e: Event<MouseData>| swipe_state.onmousemove(e),
-            onmouseup : move |e: Event<MouseData>| swipe_state.onmouseup(e, &[Dir::Up, Dir::Down]),
+            onmouseup : move |e: Event<MouseData>| {
+                swipe_state.onmouseup(e, &ALLOWED_DIRECTIONS);
+                if swipe_state.read().previous_swipe == Some(Dir::Down) {
+                    attempt_submit()
+                }
+            },
 
             div { class : "form-container",
-                h2 { "login" }
+
+                h2 { "login ↑"}
 
                 if *invalid_credentials.read() {
                     div { class : "form-error",
@@ -44,26 +74,6 @@ pub fn Login(swipe_state: Signal<swipe::State>) -> Element {
                 }
 
                 form {
-                    onsubmit : move |_| {
-                        let valid_identifier = match (
-                            Username::new(&username_or_email.read()),
-                            EmailAddress::from_str(&username_or_email.read())
-                        ) {
-                            (Ok(_), _) | (_, Ok(_)) => true,
-                            (Err(_), Err(_)) => false,
-                        };
-
-                        let valid_password = Password::new(&password.read()).is_ok();
-
-                        if !valid_identifier || !valid_password {
-                            invalid_credentials.set(true);
-                            return;
-                        }
-
-                        invalid_credentials.set(false);
-                        println!("please log me in");
-                    },
-
                     div { class : "form-group",
                         label { r#for: "identity" }
                         input {
