7c795a5dd5f0c524bf83b5a1e81ec6d10a135f72
scadoshi <scottyfermo@hotmail.com>
2025-10-06 11:33:55 -0700

    implement frontend config to load backend url from environment

--- zwiper/src/lib/config.rs
+++ zwiper/src/lib/config.rs
@@ -1,17 +1,23 @@
+use anyhow::Context;
+use reqwest::Url;
 use std::str::FromStr;
 
-use reqwest::Url;
+const BACKEND_URL_KEY: &str = "BACKEND_URL";
 
 #[derive(Debug, Clone)]
 pub struct AppConfig {
     pub backend_url: Url,
 }
 
-impl Default for AppConfig {
-    fn default() -> Self {
-        Self {
-            backend_url: Url::from_str("http://127.0.0.1:3000")
-                .expect("failed to parse default url"),
-        }
+impl AppConfig {
+    pub fn from_env() -> anyhow::Result<Self> {
+        dotenvy::dotenv().ok();
+        let backend_url_str = std::env::var(BACKEND_URL_KEY)
+            .context(format!("missing {} environment variable", BACKEND_URL_KEY))?;
+        let backend_url = Url::from_str(&backend_url_str).context(format!(
+            "invalid url in {}: {}",
+            BACKEND_URL_KEY, backend_url_str
+        ))?;
+        Ok(Self { backend_url })
     }
 }
