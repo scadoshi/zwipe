0f81ba25832f62ff87357a8ae22be46259acd474
scadoshi <scottyfermo@hotmail.com>
2025-10-05 22:11:56 -0700

    Create AppConfig with default backend URL for HTTP client configuration

--- zwiper/src/lib/config.rs
+++ zwiper/src/lib/config.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use reqwest::Url;
 
+#[derive(Debug, Clone)]
 pub struct AppConfig {
     pub backend_url: Url,
 }
@@ -9,7 +10,7 @@ pub struct AppConfig {
 impl Default for AppConfig {
     fn default() -> Self {
         Self {
-            backend_url: Url::from_str("http://localhost:3000")
+            backend_url: Url::from_str("http://127.0.0.1:3000")
                 .expect("failed to parse default url"),
         }
     }
