149121363b68eae2a360f5d312637b26ce30af28
scadoshi <scottyfermo@hotmail.com>
2025-10-06 11:22:29 -0700

    ensuring config from server is hidden from frontend - adjusted to not panic on missing .env file as could be loaded from other env sources

--- zerver/src/lib/config.rs
+++ zerver/src/lib/config.rs
@@ -1,7 +1,6 @@
+use crate::domain::auth::models::jwt::JwtSecret;
 use anyhow::Context;
 use axum::http::HeaderValue;
-
-use crate::domain::auth::models::jwt::JwtSecret;
 
 const JWT_SECRET_KEY: &str = "JWT_SECRET";
 const DATABASE_URL_KEY: &str = "DATABASE_URL";
@@ -21,14 +20,14 @@ pub struct Config {
 
 impl Config {
     pub fn from_env() -> anyhow::Result<Self> {
-        dotenvy::dotenv().context("failed to load environment")?;
-        let jwt_secret =
-            JwtSecret::new(&load_env(JWT_SECRET_KEY)?).context("invalid jwt secret from env")?;
-        let database_url = load_env(DATABASE_URL_KEY)?;
-        let bind_address = load_env(BIND_ADDRESS_KEY)?;
-        let rust_log = load_env(RUST_LOG_KEY)?;
-        let rust_backtrace = load_env(RUST_BACKTRACE_KEY)?;
-        let allowed_origins: Vec<HeaderValue> = load_env(ALLOWED_ORIGINS_KEY)?
+        dotenvy::dotenv().ok();
+        let jwt_secret = JwtSecret::new(&env_var_by_key(JWT_SECRET_KEY)?)
+            .context("invalid jwt secret from env")?;
+        let database_url = env_var_by_key(DATABASE_URL_KEY)?;
+        let bind_address = env_var_by_key(BIND_ADDRESS_KEY)?;
+        let rust_log = env_var_by_key(RUST_LOG_KEY)?;
+        let rust_backtrace = env_var_by_key(RUST_BACKTRACE_KEY)?;
+        let allowed_origins: Vec<HeaderValue> = env_var_by_key(ALLOWED_ORIGINS_KEY)?
             .split(",")
             .map(|x| x.parse())
             .collect::<Result<Vec<HeaderValue>, _>>()?;
@@ -43,6 +42,6 @@ impl Config {
     }
 }
 
-fn load_env(key: &str) -> anyhow::Result<String> {
+fn env_var_by_key(key: &str) -> anyhow::Result<String> {
     std::env::var(key).context(format!("failed to get variable from env: {}", key))
 }
