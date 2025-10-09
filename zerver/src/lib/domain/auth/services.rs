0b593a006db4bca80da4d405713d75b09c13ed76
scadoshi <scottyfermo@hotmail.com>
2025-10-09 12:59:50 -0700

    implement session service methods with cross-domain orchestration

--- zerver/src/lib/domain/auth/services.rs
+++ zerver/src/lib/domain/auth/services.rs
@@ -3,8 +3,8 @@ use crate::domain::{
         models::{
             access_token::{AccessToken, JwtSecret},
             session::{
-                CreateSession, CreateSessionError, RefreshSessionError, RevokeSessionsError,
-                Session,
+                CreateSession, CreateSessionError, DeleteExpiredSessionsError, RefreshSession,
+                RefreshSessionError, RevokeSessions, RevokeSessionsError, Session,
             },
             AuthenticateUser, AuthenticateUserError, ChangeEmail, ChangeEmailError, ChangePassword,
             ChangePasswordError, ChangeUsername, ChangeUsernameError, DeleteUser, DeleteUserError,
@@ -12,40 +12,53 @@ use crate::domain::{
         },
         ports::{AuthRepository, AuthService},
     },
-    user::models::User,
+    user::{models::User, ports::UserRepository},
 };
 use anyhow::anyhow;
 
 /// structure which implements `AuthService`
 #[derive(Debug, Clone)]
-pub struct Service<R: AuthRepository> {
-    repo: R,
+pub struct Service<AR, UR>
+where
+    AR: AuthRepository,
+    UR: UserRepository,
+{
+    auth_repo: AR,
+    user_repo: UR,
     jwt_secret: JwtSecret,
 }
 
-impl<R: AuthRepository> Service<R> {
-    pub fn new(repo: R, jwt_secret: JwtSecret) -> Self {
-        Self { repo, jwt_secret }
+impl<AR, UR> Service<AR, UR>
+where
+    AR: AuthRepository,
+    UR: UserRepository,
+{
+    pub fn new(auth_repo: AR, user_repo: UR, jwt_secret: JwtSecret) -> Self {
+        Self {
+            auth_repo,
+            user_repo,
+            jwt_secret,
+        }
     }
 }
 
-impl<R: AuthRepository + Clone> AuthService for Service<R> {
+impl<AR, UR> AuthService for Service<AR, UR>
+where
+    AR: AuthRepository + Clone,
+    UR: UserRepository + Clone,
+{
     fn jwt_secret(&self) -> &JwtSecret {
-        &self.jwt_secret // comment
+        &self.jwt_secret
     }
 
     async fn register_user(&self, request: &RegisterUser) -> Result<Session, RegisterUserError> {
-        let user = self.repo.create_user_with_password_hash(request).await?;
+        let (user, refresh_token) = self
+            .auth_repo
+            .create_user_and_refresh_token(request)
+            .await?;
 
-        let access_token = AccessToken::generate(
-            user.id,
-            user.username.clone(),
-            user.email.clone(),
-            &self.jwt_secret,
-        )
-        .map_err(|e| RegisterUserError::FailedAccessToken(anyhow!("{e}")))?;
-
-        let refresh_token = self.repo.create_refresh_token(&user.id.into()).await?;
+        let access_token = AccessToken::generate(&user, &self.jwt_secret)
+            .map_err(|e| RegisterUserError::FailedAccessToken(anyhow!("{e}")))?;
 
         Ok(Session {
             user,
@@ -59,7 +72,7 @@ impl<R: AuthRepository + Clone> AuthService for Service<R> {
         request: &AuthenticateUser,
     ) -> Result<Session, AuthenticateUserError> {
         let user_with_password_hash: UserWithPasswordHash =
-            self.repo.get_user_with_password_hash(request).await?;
+            self.auth_repo.get_user_with_password_hash(request).await?;
 
         let password_hash = user_with_password_hash.password_hash.clone();
 
@@ -73,15 +86,10 @@ impl<R: AuthRepository + Clone> AuthService for Service<R> {
             return Err(AuthenticateUserError::InvalidPassword);
         }
 
-        let access_token = AccessToken::generate(
-            user.id,
-            user.username.clone(),
-            user.email.clone(),
-            &self.jwt_secret,
-        )
-        .map_err(|e| AuthenticateUserError::FailedAccessToken(anyhow!("{e}")))?;
+        let access_token = AccessToken::generate(&user, &self.jwt_secret)
+            .map_err(|e| AuthenticateUserError::FailedAccessToken(anyhow!("{e}")))?;
 
-        let refresh_token = self.repo.create_refresh_token(&user.id.into()).await?;
+        let refresh_token = self.auth_repo.create_refresh_token(&user.id.into()).await?;
 
         Ok(Session {
             user,
@@ -91,37 +99,60 @@ impl<R: AuthRepository + Clone> AuthService for Service<R> {
     }
 
     async fn create_session(&self, request: &CreateSession) -> Result<Session, CreateSessionError> {
-        todo!()
+        let user = self.user_repo.get_user(&request.user_id).await?;
+
+        let refresh_token = self
+            .auth_repo
+            .create_refresh_token(&request.user_id)
+            .await?;
+
+        let access_token = AccessToken::generate(&user, self.jwt_secret())?;
+
+        let session = Session::new(user, access_token, refresh_token);
+
+        Ok(session)
     }
 
     async fn refresh_session(
         &self,
-        request: &CreateSession,
+        request: &RefreshSession,
     ) -> Result<Session, RefreshSessionError> {
-        todo!()
+        let user = self.user_repo.get_user(&request.user_id).await?;
+
+        let refresh_token = self.auth_repo.use_refresh_token(&request).await?;
+
+        let access_token = AccessToken::generate(&user, self.jwt_secret())?;
+
+        let session = Session::new(user, access_token, refresh_token);
+
+        Ok(session)
     }
 
-    async fn revoke_sessions(
-        &self,
-        request: &CreateSession,
-    ) -> Result<Session, RevokeSessionsError> {
-        todo!()
+    async fn revoke_sessions(&self, request: &RevokeSessions) -> Result<(), RevokeSessionsError> {
+        self.auth_repo
+            .delete_users_refresh_tokens(&request.user_id)
+            .await?;
+        Ok(())
     }
 
     async fn change_password(&self, request: &ChangePassword) -> Result<(), ChangePasswordError> {
         let _ = self.authenticate_user(&request.into()).await?;
-        self.repo.change_password(request).await
+        self.auth_repo.change_password(request).await
     }
 
     async fn change_username(&self, request: &ChangeUsername) -> Result<User, ChangeUsernameError> {
-        self.repo.change_username(request).await
+        self.auth_repo.change_username(request).await
     }
 
     async fn change_email(&self, request: &ChangeEmail) -> Result<User, ChangeEmailError> {
-        self.repo.change_email(request).await
+        self.auth_repo.change_email(request).await
     }
 
     async fn delete_user(&self, request: &DeleteUser) -> Result<(), DeleteUserError> {
-        self.repo.delete_user(request).await
+        self.auth_repo.delete_user(request).await
+    }
+
+    async fn delete_expired_sessions(&self) -> Result<(), DeleteExpiredSessionsError> {
+        self.auth_repo.delete_expired_refresh_tokens().await
     }
 }
