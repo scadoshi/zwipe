f8a87b2f272ed0a2043847d5d4d96d4646b35320
scadoshi <scottyfermo@hotmail.com>
2025-10-10 14:59:54 -0700

    Updated to support ios serving--.env is read at compile time and initialized then in const for config to use here

--- zwiper/src/lib/config.rs
+++ zwiper/src/lib/config.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 use reqwest::Url;
 use std::str::FromStr;
 
-const BACKEND_URL_KEY: &str = "BACKEND_URL";
+const BACKEND_URL: &str = env!("BACKEND_URL");
 
 #[derive(Debug, Clone)]
 pub struct AppConfig {
@@ -11,13 +11,8 @@ pub struct AppConfig {
 
 impl AppConfig {
     pub fn from_env() -> anyhow::Result<Self> {
-        dotenvy::dotenv().ok();
-        let backend_url_str = std::env::var(BACKEND_URL_KEY)
-            .context(format!("missing {} environment variable", BACKEND_URL_KEY))?;
-        let backend_url = Url::from_str(&backend_url_str).context(format!(
-            "invalid url in {}: {}",
-            BACKEND_URL_KEY, backend_url_str
-        ))?;
+        let backend_url = Url::from_str(&BACKEND_URL)
+            .context(format!("invalid url in BACKEND_URL: {}", BACKEND_URL))?;
         Ok(Self { backend_url })
     }
 }
