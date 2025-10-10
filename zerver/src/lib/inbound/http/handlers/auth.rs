fac8617b29ab95f20cdd183a032502b14d3974bd
scadoshi <scottyfermo@hotmail.com>
2025-10-10 15:24:29 -0700

    error mapping for RevokeSessions to ApiError and function for calling it from a route

--- zerver/src/lib/inbound/http/handlers/auth.rs
+++ zerver/src/lib/inbound/http/handlers/auth.rs
@@ -1,9 +1,9 @@
 use crate::domain::auth::models::access_token::InvalidJwt;
-use crate::domain::auth::models::session::RefreshSession;
 #[cfg(feature = "zerver")]
 use crate::domain::auth::models::session::{
     CreateSessionError, EnforceSessionMaximumError, InvalidRefreshSession, RefreshSessionError,
 };
+use crate::domain::auth::models::session::{RefreshSession, RevokeSessions, RevokeSessionsError};
 use crate::domain::auth::models::{AuthenticateUser, RawRegisterUser};
 #[cfg(feature = "zerver")]
 use crate::domain::auth::models::{
@@ -572,6 +572,39 @@ where
     state
         .auth_service
         .delete_user(&request)
+        .await
+        .map_err(ApiError::from)
+        .map(|_| StatusCode::NO_CONTENT)
+}
+
+// ========
+//  logout
+// ========
+
+#[cfg(feature = "zerver")]
+impl From<RevokeSessionsError> for ApiError {
+    fn from(value: RevokeSessionsError) -> Self {
+        match value {
+            RevokeSessionsError::Database(e) => e.log_500(),
+        }
+    }
+}
+
+#[cfg(feature = "zerver")]
+pub async fn revoke_sessions<AS, US, HS, CS, DS>(
+    user: AuthenticatedUser,
+    State(state): State<AppState<AS, US, HS, CS, DS>>,
+) -> Result<StatusCode, ApiError>
+where
+    AS: AuthService,
+    US: UserService,
+    HS: HealthService,
+    CS: CardService,
+    DS: DeckService,
+{
+    state
+        .auth_service
+        .revoke_sessions(&RevokeSessions::new(user.id))
         .await
         .map_err(ApiError::from)
         .map(|_| StatusCode::NO_CONTENT)
