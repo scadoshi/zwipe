dbd1cd5f637964ae830239303d2c4e8bf1c276be
scadoshi <scottyfermo@hotmail.com>
2025-10-10 21:42:32 -0700

    finished refresh client function up next is figuring out persistence for sessions and then doing logout function

--- zwiper/src/lib/http/auth.rs
+++ zwiper/src/lib/http/auth.rs
@@ -9,7 +9,7 @@ use zwipe::{
     },
     inbound::http::{
         handlers::auth::{HttpAuthenticateUser, HttpRefreshSession, HttpRegisterUser},
-        routes::{login_route, register_route},
+        routes::{login_route, logout_route, refresh_session_route, register_route},
     },
 };
 
@@ -147,12 +147,58 @@ pub async fn login(
 
 #[derive(Debug, Error)]
 pub enum RefreshError {
-    #[error("thing")]
-    Thing,
+    #[error("invalid credentials")]
+    Unauthorized,
+    #[error("access forbidden")]
+    Forbidden,
+    #[error("something went wrong")]
+    SomethingWentWrong,
+    #[error("network error")]
+    Network(reqwest::Error),
+    #[error("{0}")]
+    InvalidRequest(String),
 }
 
-pub fn refresh() -> Result<HttpRefreshSession, InvalidRefreshSession> {
-    todo!()
+impl From<reqwest::Error> for RefreshError {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Network(value)
+    }
+}
+
+impl From<serde_json::Error> for RefreshError {
+    fn from(_value: serde_json::Error) -> Self {
+        Self::SomethingWentWrong
+    }
+}
+
+pub async fn refresh(
+    request: &HttpRefreshSession,
+    auth_client: &AuthClient,
+) -> Result<Session, RefreshError> {
+    let mut url = auth_client.app_config.backend_url.clone();
+    url.set_path(&refresh_session_route());
+
+    let response = auth_client
+        .client
+        .post(url)
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&request)?)
+        .send()
+        .await?;
+
+    match response.status() {
+        StatusCode::OK => {
+            let success: Session = response.json().await?;
+            Ok(success)
+        }
+        StatusCode::UNAUTHORIZED => Err(RefreshError::Unauthorized),
+        StatusCode::FORBIDDEN => Err(RefreshError::Forbidden),
+        StatusCode::UNPROCESSABLE_ENTITY => {
+            let message = response.text().await?;
+            Err(RefreshError::InvalidRequest(message))
+        }
+        _ => Err(RefreshError::SomethingWentWrong),
+    }
 }
 
 // ========
@@ -165,6 +211,10 @@ pub enum LogoutError {
     Thing,
 }
 
-pub fn logout() -> Result<RevokeSessions, InvalidRevokeSessions> {
+pub async fn logout(auth_client: &AuthClient) -> Result<(), LogoutError> {
+    let mut url = auth_client.app_config.backend_url.clone();
+    url.set_path(&logout_route());
+
+    // x
     todo!()
 }
