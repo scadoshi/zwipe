010f2cbc626855483afe31a7ef712045abafdd1b
scadoshi <scottyfermo@hotmail.com>
2025-10-12 22:23:35 -0700

    feat: add Profile screen displaying username and email

---
+++ zwiper/src/lib/screens/main/profile.rs
@@ -0,0 +1,42 @@
+use crate::swipe::{self, Direction as Dir, OnMouse, OnTouch, VH_GAP};
+use dioxus::prelude::*;
+use zwipe::domain::auth::models::session::Session;
+
+#[component]
+pub fn Profile(swipe_state: Signal<swipe::State>) -> Element {
+    const MOVE_SWIPES: [Dir; 1] = [Dir::Up];
+    let session: Signal<Option<Session>> = use_context();
+
+    rsx! {
+        if let Some(session) = session.read().as_ref() {
+            div { class : "swipe-able",
+
+                style : format!(
+                    "transform: translateY(calc({}px - {}vh + {}vh));
+                    transition: transform {}s;",
+                    swipe_state.read().dy().from_start,
+                    VH_GAP,
+                    swipe_state.read().position.y * VH_GAP,
+                    swipe_state.read().transition_seconds
+                ),
+
+                ontouchstart : move |e: Event<TouchData>| swipe_state.ontouchstart(e),
+                ontouchmove : move |e: Event<TouchData>| swipe_state.ontouchmove(e),
+                ontouchend : move |e: Event<TouchData>| { swipe_state.ontouchend(e, &MOVE_SWIPES) },
+
+                onmousedown : move |e: Event<MouseData>| swipe_state.onmousedown(e),
+                onmousemove : move |e: Event<MouseData>| swipe_state.onmousemove(e),
+                onmouseup : move |e: Event<MouseData>| { swipe_state.onmouseup(e, &MOVE_SWIPES) },
+
+                div { class : "profile-screen",
+                    dl {
+                        dt { "Username" }
+                        dd { { session.user.username.to_string() } }
+                        dt { "Email" }
+                        dd { { session.user.email.to_string() } }
+                    }
+                }
+            }
+        }
+    }
+}
