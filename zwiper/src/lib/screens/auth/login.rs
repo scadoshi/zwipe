4e43124bd016cf5f4fed96eb1803caced4dd262a
scadoshi <scottyfermo@hotmail.com>
2025-10-12 20:55:55 -0700

    refactor: Login use session and AuthClient from context instead of props

--- zwiper/src/lib/screens/auth/login.rs
+++ zwiper/src/lib/screens/auth/login.rs
@@ -19,17 +19,19 @@ use zwipe::{
 };
 
 #[component]
-pub fn Login(swipe_state: Signal<swipe::State>, session: Signal<Option<Session>>) -> Element {
+pub fn Login(swipe_state: Signal<swipe::State>) -> Element {
     const MOVE_SWIPES: [Dir; 1] = [Dir::Up];
     const SUBMIT_SWIPE: Dir = Dir::Down;
 
-    let auth_client = use_signal(|| AuthClient::new());
+    let auth_client: Signal<AuthClient> = use_context();
+    let mut session: Signal<Option<Session>> = use_context();
 
     let mut username_or_email = use_signal(|| String::new());
     let mut password = use_signal(|| String::new());
 
     let mut submit_attempted: Signal<bool> = use_signal(|| false);
     let mut is_loading = use_signal(|| false);
+
     let mut submission_error: Signal<Option<String>> = use_signal(|| None);
 
     let valid_credentials = move || -> bool {
