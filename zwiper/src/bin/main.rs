5900c6e32611d94f1c37447e41ea09856de9926a
scadoshi <scottyfermo@hotmail.com>
2025-10-12 20:55:28 -0700

    feat: add global session context provider in App component

--- zwiper/src/bin/main.rs
+++ zwiper/src/bin/main.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
-use std::str::FromStr;
-use zwiper::{config::Config, screens::Screen};
+use zwipe::domain::auth::models::session::Session;
+use zwiper::{config::Config, screens::Screen, session::Persist};
 
 const FAVICON: Asset = asset!("/assets/favicon/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -10,12 +10,14 @@ fn main() {
     tracing_subscriber::fmt()
         .with_max_level(config.rust_log)
         .init();
-
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
+    let session: Signal<Option<Session>> = use_signal(|| Session::infallible_load());
+    use_context_provider(|| session);
+
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
