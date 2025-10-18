5f8a1c8bd9e85be212df610c9f664539a1d98a7f
scadoshi <scottyfermo@hotmail.com>
2025-10-17 18:13:32 -0700

    feat: add OnMouse trait for mouse event handling

---
+++ zwiper/src/lib/inbound/ui/components/interactions/swipe/onmouse.rs
@@ -0,0 +1,29 @@
+use crate::inbound::ui::components::interactions::swipe::{config::SwipeConfig, state::SwipeState};
+use dioxus::{html::input_data::MouseButton, prelude::*};
+
+// wiring event for mouse interactions to swipe behavior
+// only used while developing on desktop emulation
+pub trait OnMouse {
+    fn onmousedown(&mut self, e: Event<MouseData>);
+    fn onmousemove(&mut self, e: Event<MouseData>, config: &SwipeConfig);
+    fn onmouseup(&mut self, e: Event<MouseData>, config: &SwipeConfig);
+}
+
+impl OnMouse for Signal<SwipeState> {
+    fn onmousedown(&mut self, e: Event<MouseData>) {
+        let start_point = e.client_coordinates();
+        self.with_mut(|ss| ss.onswipestart(start_point));
+    }
+
+    fn onmousemove(&mut self, e: Event<MouseData>, config: &SwipeConfig) {
+        if e.held_buttons().contains(MouseButton::Primary) {
+            let current_point = e.client_coordinates();
+            self.with_mut(|ss| ss.onswipemove(current_point, config));
+        }
+    }
+
+    fn onmouseup(&mut self, e: Event<MouseData>, config: &SwipeConfig) {
+        let end_point = e.client_coordinates();
+        self.with_mut(|ss| ss.onswipeend(end_point, config));
+    }
+}
