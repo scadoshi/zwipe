47a162c94fbd5c764d2518fb52bd896645b8f6b2
scadoshi <scottyfermo@hotmail.com>
2025-10-17 18:13:43 -0700

    refactor: comment out app home route for focused auth development

--- zwiper/src/lib/inbound/ui.rs
+++ zwiper/src/lib/inbound/ui.rs
@@ -1,15 +1,16 @@
 pub mod components;
 
 use crate::inbound::ui::components::screens::{
-    app::home::Home as AppHome, auth::home::Home as AuthHome,
+    // app::home::Home as AppHome,
+    auth::home::Home as AuthHome,
 };
 use dioxus::prelude::*;
 
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
-pub enum Screen {
-    #[route("/")]
-    AppHome {},
-    #[route("/auth")]
+pub enum Router {
+    // #[route("/")]
+    // AppHome {},
+    #[route("/")] // change this back to auth once ready
     AuthHome {},
 }
