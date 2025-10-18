f8cc4b33729bdaa76a8fff0bffeeb254ba218f44
scadoshi <scottyfermo@hotmail.com>
2025-10-17 18:13:37 -0700

    feat: integrate Swipeable in auth home with shared state

--- zwiper/src/lib/inbound/ui/components/screens/auth/home.rs
+++ zwiper/src/lib/inbound/ui/components/screens/auth/home.rs
@@ -3,41 +3,23 @@ use zwipe::domain::{auth::models::session::Session, logo};
 
 use crate::inbound::ui::components::{
     interactions::swipe::{
-        direction::Direction, onswipe::OnMouse, ontouch::OnTouch, state::SwipeState, VH_GAP,
+        config::SwipeConfig, direction::Direction, onmouse::OnMouse, ontouch::OnTouch,
+        state::SwipeState, Swipeable, VH_GAP,
     },
     screens::auth::{login::Login, register::Register},
 };
 
 #[component]
 pub fn Home() -> Element {
-    const MOVE_SWIPES: [Direction; 2] = [Direction::Up, Direction::Down];
-
-    let session: Signal<Option<Session>> = use_context();
-    let mut swipe_state = use_signal(|| SwipeState::new());
+    let swipe_state = use_signal(|| SwipeState::new());
+    let swipe_config: SwipeConfig =
+        SwipeConfig::new(vec![Direction::Up, Direction::Down], None, None);
 
     let logo = logo::logo();
 
     rsx! {
-        if session.read().is_none() { Login {swipe_state} }
-
-        div { class : "swipe-able",
-
-            style : format!(
-                "transform: translateY(calc({}px + {}vh));
-                transition: transform {}s;",
-                swipe_state.read().dy().from_start,
-                swipe_state.read().position.y * VH_GAP,
-                swipe_state.read().transition_seconds
-            ),
-
-            ontouchstart : move |e: Event<TouchData>| swipe_state.ontouchstart(e),
-            ontouchmove : move |e: Event<TouchData>| swipe_state.ontouchmove(e),
-            ontouchend : move |e: Event<TouchData>| swipe_state.ontouchend(e, &MOVE_SWIPES),
-
-            onmousedown : move |e: Event<MouseData>| swipe_state.onmousedown(e),
-            onmousemove : move |e: Event<MouseData>| swipe_state.onmousemove(e),
-            onmouseup : move |e: Event<MouseData>| swipe_state.onmouseup(e, &MOVE_SWIPES),
-
+        Login { swipe_state }
+        Swipeable { state: swipe_state, config: swipe_config,
             div { class : "home-screen",
 
                 div { class : "home-up-hint",
@@ -53,7 +35,6 @@ pub fn Home() -> Element {
                 },
             }
         }
-
-        if session.read().is_none() { Register {swipe_state} }
+        Register { swipe_state }
     }
 }
