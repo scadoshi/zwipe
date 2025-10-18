6da7d3c4dd79ba53725f098dc931fce0d065db90
scadoshi <scottyfermo@hotmail.com>
2025-10-17 18:13:44 -0700

    refactor: revert to use_context_provider for session and auth client

--- zwiper/src/bin/main.rs
+++ zwiper/src/bin/main.rs
@@ -1,21 +1,18 @@
 use dioxus::prelude::*;
 use zwipe::domain::auth::models::session::Session;
-use zwiper::{config::Config, domain::development::Spoof};
+use zwiper::{
+    config::Config,
+    domain::development::Spoof,
+    inbound::ui::Router,
+    outbound::{client::auth::AuthClient, session::Persist},
+};
 
 const FAVICON: Asset = asset!("/assets/favicon/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 
-static SESSION: GlobalSignal<Option<Session>> = Signal::global(|| {
-    // use this later when actual persistence is achieved
-    // Session::infallible_load()
-    // for now we can just spoof it
-    Some(Session::spoof())
-});
-
-static AUTH_CLIENT: GlobalSignal<AuthClient> = Signal::global(|| AuthClient::new());
-
 fn main() {
     let config = Config::from_env();
+
     tracing_subscriber::fmt()
         .with_max_level(config.rust_log)
         .init();
@@ -24,9 +21,21 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    let session: Signal<Option<Session>> = use_signal(|| {
+        // use this later when actual pesrsistence is achieved
+        // Session::infallible_load()
+        // for now we can just spoof it
+        Some(Session::spoof())
+    });
+    use_context_provider(|| session);
+
+    let auth_client: Signal<AuthClient> = use_signal(|| AuthClient::new());
+    use_context_provider(|| auth_client);
+
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Router::<Screen> {}
+
+        Router::<Router> {}
     }
 }
