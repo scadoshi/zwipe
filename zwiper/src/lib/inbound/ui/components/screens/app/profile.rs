2e331f7a45699a0a732cb0fa96f0ab1a5ef55b35
scadoshi <scottyfermo@hotmail.com>
2025-10-18 14:00:36 -0700

    remove duplicate session validation from profile component

--- zwiper/src/lib/inbound/ui/components/screens/app/profile.rs
+++ zwiper/src/lib/inbound/ui/components/screens/app/profile.rs
@@ -1,12 +1,7 @@
-use crate::{
-    inbound::ui::components::interactions::swipe::{
-        config::SwipeConfig, direction::Direction as Dir, state::SwipeState, Swipeable,
-    },
-    outbound::client::auth::{session::ActiveSession, AuthClient},
+use crate::inbound::ui::components::interactions::swipe::{
+    config::SwipeConfig, direction::Direction as Dir, state::SwipeState, Swipeable,
 };
 use dioxus::prelude::*;
-use std::time::Duration;
-use tokio::time::interval;
 use zwipe::domain::auth::models::session::Session;
 
 #[component]
@@ -16,25 +11,8 @@ pub fn Profile(swipe_state: Signal<SwipeState>) -> Element {
         submission_swipe: None,
         from_main_screen: Some(Dir::Up),
     };
-    let auth_client: Signal<AuthClient> = use_context();
-    let mut session: Signal<Option<Session>> = use_context();
 
-    let check_session = move || async move {
-        let Some(s) = session.read().clone() else {
-            return;
-        };
-        session.set(auth_client.read().infallible_get_active_session(&s).await);
-    };
-
-    use_effect(move || {
-        spawn(async move {
-            let mut interval = interval(Duration::from_secs(60));
-            loop {
-                interval.tick().await;
-                check_session().await;
-            }
-        });
-    });
+    let session: Signal<Option<Session>> = use_context();
 
     rsx! {
         if let Some(session) = session.read().as_ref() {
