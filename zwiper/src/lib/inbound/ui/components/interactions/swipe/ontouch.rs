b16aebc1c7cb4049e5ea4e5e7bf6a6acdccd1f71
scadoshi <scottyfermo@hotmail.com>
2025-10-17 18:13:35 -0700

    refactor: update OnTouch trait to pass SwipeConfig

--- zwiper/src/lib/inbound/ui/components/interactions/swipe/ontouch.rs
+++ zwiper/src/lib/inbound/ui/components/interactions/swipe/ontouch.rs
@@ -1,34 +1,32 @@
-use crate::inbound::ui::components::interactions::swipe::{
-    direction::Direction, state::SwipeState,
-};
+use crate::inbound::ui::components::interactions::swipe::{config::SwipeConfig, state::SwipeState};
 use dioxus::prelude::*;
 
 // wiring event for touch interactions to swipe behavior
 pub trait OnTouch {
     fn ontouchstart(&mut self, e: Event<TouchData>);
-    fn ontouchmove(&mut self, e: Event<TouchData>);
-    fn ontouchend(&mut self, e: Event<TouchData>, swipe_dirs: &[Direction]);
+    fn ontouchmove(&mut self, e: Event<TouchData>, config: &SwipeConfig);
+    fn ontouchend(&mut self, e: Event<TouchData>, config: &SwipeConfig);
 }
 
 impl OnTouch for Signal<SwipeState> {
     fn ontouchstart(&mut self, e: Event<TouchData>) {
         if let Some(t) = e.touches().into_iter().next() {
-            let point = t.client_coordinates();
-            self.with_mut(|ss| ss.onswipestart(point));
+            let start_point = t.client_coordinates();
+            self.with_mut(|ss| ss.onswipestart(start_point));
         }
     }
 
-    fn ontouchmove(&mut self, e: Event<TouchData>) {
+    fn ontouchmove(&mut self, e: Event<TouchData>, config: &SwipeConfig) {
         if let Some(t) = e.touches().into_iter().next() {
-            let point = t.client_coordinates();
-            self.with_mut(|ss| ss.onswipemove(point));
+            let current_point = t.client_coordinates();
+            self.with_mut(|ss| ss.onswipemove(current_point, config));
         }
     }
 
-    fn ontouchend(&mut self, e: Event<TouchData>, swipe_dirs: &[Direction]) {
+    fn ontouchend(&mut self, e: Event<TouchData>, config: &SwipeConfig) {
         if let Some(t) = e.touches_changed().into_iter().next() {
-            let point = t.client_coordinates();
-            self.with_mut(|ss| ss.onswipeend(point, swipe_dirs));
+            let end_point = t.client_coordinates();
+            self.with_mut(|ss| ss.onswipeend(end_point, config));
         }
     }
 }
