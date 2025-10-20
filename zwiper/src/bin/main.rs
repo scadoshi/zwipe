d2e361595b006b2cce4f71daa0d73020bdfe2f71
scadoshi <scottyfermo@hotmail.com>
2025-10-19 22:36:11 -0700

    Add SessionProvider component wrapping Router for centralized session management

--- zwiper/src/bin/main.rs
+++ zwiper/src/bin/main.rs
@@ -1,10 +1,7 @@
 use dioxus::prelude::*;
-use zwipe::domain::auth::models::session::Session;
 use zwiper::{
     config::Config,
-    domain::development::Spoof,
-    inbound::ui::Router,
-    outbound::{client::auth::AuthClient, session::Persist},
+    inbound::ui::{components::session_supplier::SessionSupplier, router::Router},
 };
 
 const FAVICON: Asset = asset!("/assets/favicon/favicon.ico");
@@ -12,7 +9,6 @@ const MAIN_CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     let config = Config::from_env();
-
     tracing_subscriber::fmt()
         .with_max_level(config.rust_log)
         .init();
@@ -21,21 +17,12 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    let session: Signal<Option<Session>> = use_signal(|| {
-        // use this later when actual persistence is achieved
-        Session::infallible_load()
-        // for now we can just spoof it
-        // Some(Session::spoof())
-    });
-    use_context_provider(|| session);
-
-    let auth_client: Signal<AuthClient> = use_signal(|| AuthClient::new());
-    use_context_provider(|| auth_client);
-
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
 
-        Router::<Router> {}
+        SessionSupplier {
+            Router::<Router> {}
+        }
     }
 }
