5a4f532cba7531a4d6c72de59353d6142ed046b6
scadoshi <scottyfermo@hotmail.com>
2025-10-21 16:43:16 -0700

    integrate Bouncer and SignalLogout trait in profile screen

--- zwiper/src/lib/inbound/ui/components/screens/app/profile.rs
+++ zwiper/src/lib/inbound/ui/components/screens/app/profile.rs
@@ -4,10 +4,14 @@ pub mod change_username;
 
 use crate::{
     inbound::ui::{
-        components::interactions::swipe::{config::SwipeConfig, state::SwipeState, Swipeable},
+        components::{
+            auth::bouncer::Bouncer,
+            interactions::swipe::{config::SwipeConfig, state::SwipeState, Swipeable},
+            screens::app::signal_logout::SignalLogout,
+        },
         router::Router,
     },
-    outbound::session::Persist,
+    outbound::client::auth::AuthClient,
 };
 use dioxus::prelude::*;
 use zwipe::domain::auth::models::session::Session;
@@ -17,75 +21,72 @@ pub fn Profile() -> Element {
     let swipe_state = use_signal(|| SwipeState::new());
     let swipe_config = SwipeConfig::blank();
 
-    let mut session: Signal<Option<Session>> = use_context();
+    let session: Signal<Option<Session>> = use_context();
+    let auth_client: Signal<AuthClient> = use_context();
+
     let navigator = use_navigator();
 
     rsx! {
         if let Some(active_sesh) = session.read().as_ref() {
-            Swipeable { state: swipe_state, config: swipe_config,
-                div { class : "profile-container",
-                    h2 { "profile" }
+            Bouncer {
+                Swipeable { state: swipe_state, config: swipe_config,
+                    div { class : "profile-container",
+                        h2 { "profile" }
 
-                    div { class : "profile-field",
-                        div { class : "profile-field-content",
-                            label { "username" }
-                            p { { active_sesh.user.username.to_string() } }
-                        }
-                        button {
-                            class: "profile-field-button",
-                            onclick : move |_| {
-                                navigator.push(Router::ChangeUsername {});
-                            },
-                            "change"
-                        }
-                    }
-
-                    div { class : "profile-field",
-                        div { class : "profile-field-content",
-                            label { "email" }
-                            p { { active_sesh.user.email.to_string() } }
-                        }
-                        button {
-                            class: "profile-field-button",
-                            onclick : move |_| {
-                                navigator.push(Router::ChangeEmail {});
-                            },
-                            "change"
-                        }
-                    }
-
-                    div { class : "profile-field",
-                        div { class : "profile-field-content",
-                            label { "password" }
-                            p { "•••••••" }
-                        }
-                        button {
-                            class: "profile-field-button",
-                            onclick : move |_| {
-                                navigator.push(Router::ChangePassword {});
-                            },
-                            "change"
-                        }
-                    }
-
-                    button {
-                        class: "logout-button",
-                        onclick : move |_| {
-                            // todo: Call logout endpoint on server to invalidate refresh token
-                            if let Some(current_session) = session.read().clone() {
-                                let _ = current_session.delete();
+                        div { class : "profile-field",
+                            div { class : "profile-field-content",
+                                label { "username" }
+                                p { { active_sesh.user.username.to_string() } }
                             }
-                            session.set(None);
-                            navigator.push(Router::Login {});
-                        },
-                        "logout"
-                    }
+                            button {
+                                class: "profile-field-button",
+                                onclick : move |_| {
+                                    navigator.push(Router::ChangeUsername {});
+                                },
+                                "change"
+                            }
+                        }
 
-                    button {
-                        onclick : move |_| {
-                            navigator.push(Router::Home {});
-                        },
-                        "back"
+                        div { class : "profile-field",
+                            div { class : "profile-field-content",
+                                label { "email" }
+                                p { { active_sesh.user.email.to_string() } }
+                            }
+                            button {
+                                class: "profile-field-button",
+                                onclick : move |_| {
+                                    navigator.push(Router::ChangeEmail {});
+                                },
+                                "change"
+                            }
+                        }
+
+                        div { class : "profile-field",
+                            div { class : "profile-field-content",
+                                label { "password" }
+                                p { "•••••••" }
+                            }
+                            button {
+                                class: "profile-field-button",
+                                onclick : move |_| {
+                                    navigator.push(Router::ChangePassword {});
+                                },
+                                "change"
+                            }
+                        }
+
+                        button {
+                            class: "logout-button",
+                            onclick : move |_| session.logout(auth_client),
+                            "logout"
+                        }
+
+                        button {
+                            onclick : move |_| {
+                                navigator.push(Router::Home {});
+                            },
+                            "back"
+                        }
                     }
                 }
             }
