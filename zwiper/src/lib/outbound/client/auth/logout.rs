5c4bba03634901eeb9a3077002a1f936fb087a22
scadoshi <scottyfermo@hotmail.com>
2025-10-20 21:43:44 -0700

    implement logout HTTP client with bearer token authentication

--- zwiper/src/lib/outbound/client/auth/logout.rs
+++ zwiper/src/lib/outbound/client/auth/logout.rs
@@ -1,25 +1,49 @@
+use reqwest::StatusCode;
 use std::future::Future;
+use thiserror::Error;
+use zwipe::{domain::auth::models::session::Session, inbound::http::routes::logout_route};
 
 use crate::outbound::client::auth::AuthClient;
-use thiserror::Error;
-use zwipe::inbound::http::routes::logout_route;
 
 #[derive(Debug, Error)]
 pub enum LogoutError {
-    #[error("thing")]
-    Thing,
+    #[error("unauthorized")]
+    Unauthorized,
+    #[error("something went wrong")]
+    SomethingWentWrong,
+    #[error("network error")]
+    Network(reqwest::Error),
+}
+
+impl From<reqwest::Error> for LogoutError {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Network(value)
+    }
 }
 
 pub trait Logout {
-    fn logout(auth_client: &AuthClient) -> impl Future<Output = Result<(), LogoutError>> + Send;
+    fn logout(&self, session: &Session) -> impl Future<Output = Result<(), LogoutError>> + Send;
 }
 
 impl Logout for AuthClient {
-    async fn logout(auth_client: &AuthClient) -> Result<(), LogoutError> {
-        let mut url = auth_client.app_config.backend_url.clone();
+    async fn logout(&self, session: &Session) -> Result<(), LogoutError> {
+        let mut url = self.app_config.backend_url.clone();
         url.set_path(&logout_route());
 
-        // x
-        todo!()
+        let response = self
+            .client
+            .post(url)
+            .header(
+                "Authorization",
+                format!("Bearer {}", session.access_token.value.as_str()),
+            )
+            .send()
+            .await?;
+
+        match response.status() {
+            StatusCode::NO_CONTENT | StatusCode::OK => Ok(()),
+            StatusCode::UNAUTHORIZED => Err(LogoutError::Unauthorized),
+            _ => Err(LogoutError::SomethingWentWrong),
+        }
     }
 }
