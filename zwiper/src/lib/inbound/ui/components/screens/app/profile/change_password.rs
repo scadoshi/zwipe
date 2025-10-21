9225343f5a964306b71a1d0ee6aade84ef123532
scadoshi <scottyfermo@hotmail.com>
2025-10-21 16:43:33 -0700

    extract form submission logic and add input clearing in change_password

--- zwiper/src/lib/inbound/ui/components/screens/app/profile/change_password.rs
+++ zwiper/src/lib/inbound/ui/components/screens/app/profile/change_password.rs
@@ -1,14 +1,15 @@
 use crate::{
     inbound::ui::{
         components::{
+            auth::bouncer::Bouncer,
             interactions::swipe::{config::SwipeConfig, state::SwipeState, Swipeable},
-            success_messages::get_random_success_message,
+            success_messages::random_success_message,
         },
         router::Router,
     },
     outbound::client::auth::{
-        change_password::{ChangePassword as ChangePasswordTrait, ChangePasswordError},
-        session::ActiveSession,
+        change_password::{AuthClientChangePassword, ChangePasswordError},
+        session::AuthClientSession,
         AuthClient,
     },
 };
@@ -28,9 +29,9 @@ pub fn ChangePassword() -> Element {
     let mut session: Signal<Option<Session>> = use_context();
     let auth_client: Signal<AuthClient> = use_context();
 
-    // we do not validate current in case their current
-    // somehow doesn't follow password policies
-    // we don't want to lock them to it
+    // we do not validate current password on frontend
+    // as to not lock them out of changing
+    // if their current somehow defies policy
     let mut current_password = use_signal(|| String::new());
 
     let mut new_password = use_signal(|| String::new());
@@ -52,115 +53,126 @@ pub fn ChangePassword() -> Element {
 
     let mut inputs_are_valid = move || {
         validate_new_password();
-        Password::new(&current_password.read()).is_ok() && password_error.read().is_none()
+        password_error.read().is_none()
+    };
+
+    let mut clear_inputs = move || {
+        current_password.set(String::new());
+        new_password.set(String::new());
+        confirm_password.set(String::new());
+    };
+
+    let mut attempt_submit = move || {
+        submit_attempted.set(true);
+        if inputs_are_valid() {
+            tracing::info!("change password");
+            let request = HttpChangePassword::new(&*current_password.read(), &*new_password.read());
+            spawn(async move {
+                let Some(current) = session.read().clone() else {
+                    submission_error.set(Some(ChangePasswordError::SessionExpired.to_string()));
+                    return;
+                };
+
+                let Some(active) = auth_client
+                    .read()
+                    .infallible_get_active_session(&current)
+                    .await
+                else {
+                    return;
+                };
+
+                match auth_client.read().change_password(request, &active).await {
+                    Ok(()) => {
+                        success_message.set(Some(random_success_message()));
+                        submission_error.set(None);
+                        clear_inputs();
+                    }
+                    Err(e) => submission_error.set(Some(e.to_string())),
+                }
+
+                if active != current {
+                    session.set(Some(active));
+                }
+            });
+        } else {
+            submission_error.set(Some("invalid input".to_string()));
+        }
     };
 
     rsx! {
-        Swipeable { state: swipe_state, config: swipe_config,
-            div { class : "form-container",
+        Bouncer {
+            Swipeable { state: swipe_state, config: swipe_config,
+                div { class : "form-container",
 
-                h2 { "change password" }
+                    h2 { "change password" }
 
-                form {
-                    div { class : "form-group",
+                    form {
+                        div { class : "form-group",
 
-                        input {
-                            id : "current_password",
-                            r#type : "password",
-                            placeholder : "current",
-                            value : "{current_password}",
-                            autocapitalize : "none",
-                            spellcheck : "false",
-                            oninput: move |event| {
-                                current_password.set(event.value());
-                            }
-                        }
-
-                        if *submit_attempted.read() {
-                            if let Some(error) = password_error.read().as_ref() {
-                                div { class : "error", "{error}" }
-                            }
-                        }
-
-                        input {
-                            id : "new_password",
-                            r#type : "password",
-                            placeholder : "new",
-                            value : "{new_password}",
-                            autocapitalize : "none",
-                            spellcheck : "false",
-                            oninput : move |event| {
-                                new_password.set(event.value());
-                                if *submit_attempted.read() {
-                                    validate_new_password();
+                            input {
+                                id : "current_password",
+                                r#type : "password",
+                                placeholder : "current",
+                                value : "{current_password}",
+                                autocapitalize : "none",
+                                spellcheck : "false",
+                                oninput: move |event| {
+                                    current_password.set(event.value());
                                 }
                             }
-                        }
 
-                        input {
-                            id : "confirm_password",
-                            r#type : "password",
-                            placeholder : "confirm new",
-                            value : "{confirm_password}",
-                            autocapitalize : "none",
-                            spellcheck : "false",
-                            oninput : move |event| {
-                                confirm_password.set(event.value());
-                                if *submit_attempted.read() {
-                                    validate_new_password();
+                            if *submit_attempted.read() {
+                                if let Some(error) = password_error.read().as_ref() {
+                                    div { class : "error", "{error}" }
                                 }
                             }
-                        }
 
-                        button {
-                            onclick : move |_| {
-                                submit_attempted.set(true);
-                                if inputs_are_valid() {
-                                    tracing::info!("change password");
-                                    let request = HttpChangePassword::new(
-                                        &*current_password.read(),
-                                        &*new_password.read()
-                                    );
-                                    spawn(async move {
-                                        let Some(current) = session.read().clone() else {
-                                            submission_error.set(Some(ChangePasswordError::SessionExpired.to_string()));
-                                            return;
-                                        };
-
-                                        let Some(active) = auth_client
-                                            .read()
-                                            .infallible_get_active_session(&current)
-                                            .await else { return; };
-
-                                        match auth_client.read().change_password(request, &active).await {
-                                            Ok(()) => {
-                                                success_message.set(Some(get_random_success_message()));
-                                                submission_error.set(None);
-                                            }
-                                            Err(e) => submission_error.set(Some(e.to_string())),
-                                        }
-
-                                        if active != current {
-                                            session.set(Some(active));
-                                        }
-                                    });
-
-                                } else {
-                                    submission_error.set(Some("invalid input".to_string()));
+                            input {
+                                id : "new_password",
+                                r#type : "password",
+                                placeholder : "new",
+                                value : "{new_password}",
+                                autocapitalize : "none",
+                                spellcheck : "false",
+                                oninput : move |event| {
+                                    new_password.set(event.value());
+                                    if *submit_attempted.read() {
+                                        validate_new_password();
+                                    }
                                 }
-                            }, "submit"
-                        }
+                            }
 
-                        if let Some(error) = submission_error.read().as_deref() {
-                            div { class: "error", "{error}" }
-                        } else if let Some(success_message) = success_message.read().as_deref() {
-                            div { class: "success-message", {success_message} }
-                        }
+                            input {
+                                id : "confirm_password",
+                                r#type : "password",
+                                placeholder : "confirm new",
+                                value : "{confirm_password}",
+                                autocapitalize : "none",
+                                spellcheck : "false",
+                                oninput : move |event| {
+                                    confirm_password.set(event.value());
+                                    if *submit_attempted.read() {
+                                        validate_new_password();
+                                    }
+                                }
+                            }
 
-                        button {
-                            onclick : move |_| {
-                                navigator.push(Router::Profile {});
-                            }, "back"
+                            button {
+                                onclick : move |_| attempt_submit(),
+                                "submit"
+                            }
+
+                            if let Some(error) = submission_error.read().as_deref() {
+                                div { class: "error", "{error}" }
+                            } else if let Some(success_message) = success_message.read().as_deref() {
+                                div { class: "success-message", {success_message} }
+                            }
+
+                            button {
+                                onclick : move |_| {
+                                    navigator.push(Router::Profile {});
+                                }, "back"
+                            }
                         }
                     }
                 }
