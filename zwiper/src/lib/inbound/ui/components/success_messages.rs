e8f2a4c558b52184f147d699a0f6f9ffc79195fb
scadoshi <scottyfermo@hotmail.com>
2025-10-21 16:42:57 -0700

    rename function and add more concise success messages

--- zwiper/src/lib/inbound/ui/components/success_messages.rs
+++ zwiper/src/lib/inbound/ui/components/success_messages.rs
@@ -1,19 +1,24 @@
-const SUCCESS_MESSAGES: [&str; 10] = [
-    "bazinga! your data has been transmogrified into digital gold!",
-    "eureka! the cosmic alignment of your request has been achieved!",
-    "shazam! your magical incantation has summoned the perfect response!",
-    "kapow! your data has been transformed with the power of a thousand suns!",
-    "wham! your request has been processed with the force of a neutron star!",
-    "boom! your data has been launched into the success stratosphere!",
-    "ding! your submission has been accelerated to light speed!",
-    "pow! your magical gathering has summoned the ultimate response!",
-    "bam! your request has been processed with the precision of a quantum computer!",
-    "zap! your submission has been electrified into digital perfection!",
+const SUCCESS_MESSAGES: [&str; 15] = [
+    "bazinga!",
+    "shazam!",
+    "eureka!",
+    "five booms!",
+    "pow!",
+    "excelsior!",
+    "up up and away!",
+    "beamed up!",
+    "positively stupendous!",
+    "success has been achieved!",
+    "victory has been attained!",
+    "dominance has been established!",
+    "information transmission received!",
+    "configuration transfer protocol sent!",
+    "your request has ascended into the heavens!",
 ];
 
 use rand::Rng;
 
-pub fn get_random_success_message() -> String {
+pub fn random_success_message() -> String {
     let mut rng = rand::rng();
     let index = rng.random_range(0..SUCCESS_MESSAGES.len());
     SUCCESS_MESSAGES[index].to_string()
