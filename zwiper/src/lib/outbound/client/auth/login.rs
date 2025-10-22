93ad8a207cad0291dc9f4f17916dff8079b69a2c
scadoshi <scottyfermo@hotmail.com>
2025-10-21 23:08:33 -0700

    refactor login register logout to use apierror and json convenience methods

--- zwiper/src/lib/outbound/client/auth/login.rs
+++ zwiper/src/lib/outbound/client/auth/login.rs
@@ -1,69 +1,35 @@
-use crate::outbound::client::auth::AuthClient;
+use crate::outbound::client::{auth::AuthClient, error::ApiError};
 use reqwest::StatusCode;
 use std::future::Future;
-use thiserror::Error;
 use zwipe::{
     domain::auth::models::session::Session,
     inbound::http::{handlers::auth::authenticate_user::HttpAuthenticateUser, routes::login_route},
 };
 
-#[derive(Debug, Error)]
-pub enum LoginError {
-    #[error("invalid credentials")]
-    Unauthorized,
-    #[error("something went wrong")]
-    SomethingWentWrong,
-    #[error("network error")]
-    Network(reqwest::Error),
-    #[error("{0}")]
-    InvalidRequest(String),
-}
-
-impl From<reqwest::Error> for LoginError {
-    fn from(value: reqwest::Error) -> Self {
-        Self::Network(value)
-    }
-}
-
-impl From<serde_json::Error> for LoginError {
-    fn from(_value: serde_json::Error) -> Self {
-        Self::SomethingWentWrong
-    }
-}
-
 pub trait AuthClientLogin {
     fn authenticate_user(
         &self,
         request: HttpAuthenticateUser,
-    ) -> impl Future<Output = Result<Session, LoginError>> + Send;
+    ) -> impl Future<Output = Result<Session, ApiError>> + Send;
 }
 
 impl AuthClientLogin for AuthClient {
-    async fn authenticate_user(
-        &self,
-        request: HttpAuthenticateUser,
-    ) -> Result<Session, LoginError> {
+    async fn authenticate_user(&self, request: HttpAuthenticateUser) -> Result<Session, ApiError> {
         let mut url = self.app_config.backend_url.clone();
         url.set_path(&login_route());
-        let response = self
-            .client
-            .post(url)
-            .header("Content-Type", "application/json")
-            .body(serde_json::to_string(&request)?)
-            .send()
-            .await?;
+        let response = self.client.post(url).json(&request).send().await?;
 
-        match response.status() {
+        let status = response.status();
+
+        match status {
             StatusCode::OK => {
-                let success: Session = response.json().await?;
-                Ok(success)
+                let new: Session = response.json().await?;
+                Ok(new)
             }
-            StatusCode::UNPROCESSABLE_ENTITY => {
+            _ => {
                 let message = response.text().await?;
-                Err(LoginError::InvalidRequest(message))
+                Err((status, message).into())
             }
-            StatusCode::UNAUTHORIZED => Err(LoginError::Unauthorized),
-            _ => Err(LoginError::SomethingWentWrong),
         }
     }
 }
--- zwiper/src/lib/outbound/client/auth/logout.rs
+++ zwiper/src/lib/outbound/client/auth/logout.rs
@@ -1,49 +1,32 @@
+use crate::outbound::client::{auth::AuthClient, error::ApiError};
 use reqwest::StatusCode;
 use std::future::Future;
-use thiserror::Error;
 use zwipe::{domain::auth::models::session::Session, inbound::http::routes::logout_route};
 
-use crate::outbound::client::auth::AuthClient;
-
-#[derive(Debug, Error)]
-pub enum LogoutError {
-    #[error("unauthorized")]
-    Unauthorized,
-    #[error("something went wrong")]
-    SomethingWentWrong,
-    #[error("network error")]
-    Network(reqwest::Error),
-}
-
-impl From<reqwest::Error> for LogoutError {
-    fn from(value: reqwest::Error) -> Self {
-        Self::Network(value)
-    }
-}
-
 pub trait AuthClientLogout {
-    fn logout(&self, session: &Session) -> impl Future<Output = Result<(), LogoutError>> + Send;
+    fn logout(&self, session: &Session) -> impl Future<Output = Result<(), ApiError>> + Send;
 }
 
 impl AuthClientLogout for AuthClient {
-    async fn logout(&self, session: &Session) -> Result<(), LogoutError> {
+    async fn logout(&self, session: &Session) -> Result<(), ApiError> {
         let mut url = self.app_config.backend_url.clone();
         url.set_path(&logout_route());
 
         let response = self
             .client
             .post(url)
-            .header(
-                "Authorization",
-                format!("Bearer {}", session.access_token.value.as_str()),
-            )
+            .bearer_auth(session.access_token.value.as_str())
             .send()
             .await?;
 
-        match response.status() {
+        let status = response.status();
+
+        match status {
             StatusCode::NO_CONTENT | StatusCode::OK => Ok(()),
-            StatusCode::UNAUTHORIZED => Err(LogoutError::Unauthorized),
-            _ => Err(LogoutError::SomethingWentWrong),
+            _ => {
+                let message = response.text().await?;
+                Err((status, message).into())
+            }
         }
     }
 }
