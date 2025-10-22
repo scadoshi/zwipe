d3c0913e48632b97d0be21b0669e11238a78b5f9
scadoshi <scottyfermo@hotmail.com>
2025-10-22 15:57:45 -0700

    refactor logo system with modular Zwipe, Zerver, Zervice, Zwiper variants

--- zerver/src/lib/domain/logo.rs
+++ zerver/src/lib/domain/logo.rs
@@ -1,7 +1,35 @@
-pub fn print() {
-    println!("{}", logo());
+pub const ZWIPE: &str = include_str!("logo/zwipe.txt");
+pub struct Zwipe;
+
+impl Zwipe {
+    pub fn print() {
+        println!("{ZWIPE}");
+    }
 }
 
-pub fn logo() -> &'static str {
-    include_str!("logo/1.txt")
+pub const ZERVER: &str = include_str!("logo/zerver.txt");
+pub struct Zerver;
+
+impl Zerver {
+    pub fn print() {
+        println!("{ZERVER}");
+    }
+}
+
+pub const ZERVICE: &str = include_str!("logo/zervice.txt");
+pub struct Zervice;
+
+impl Zervice {
+    pub fn print() {
+        println!("{ZERVICE}");
+    }
+}
+
+pub const ZWIPER: &str = include_str!("logo/zwiper.txt");
+pub struct Zwiper;
+
+impl Zwiper {
+    pub fn print() {
+        println!("{ZWIPER}");
+    }
 }
