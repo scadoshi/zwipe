a5cecb60d504d6fb911d98ca65a2409b54d850e9
scadoshi <scottyfermo@hotmail.com>
2025-11-26 10:00:22 -0700

    feat: add GetScryfallDataError to CreateCardError for delta sync support

--- zerver/src/lib/domain/card/models/create_card.rs
+++ zerver/src/lib/domain/card/models/create_card.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "zerver")]
+use crate::domain::card::models::scryfall_data::get_scryfall_data::GetScryfallDataError;
+#[cfg(feature = "zerver")]
 use thiserror::Error;
 
 #[cfg(feature = "zerver")]
@@ -12,4 +14,13 @@ pub enum CreateCardError {
     ScryfallDataFromDb(anyhow::Error),
     #[error("card profile created but database returned invalid object: {0}")]
     CardProfileFromDb(anyhow::Error),
+    #[error(transparent)]
+    GetScryfallData(anyhow::Error),
+}
+
+#[cfg(feature = "zerver")]
+impl From<GetScryfallDataError> for CreateCardError {
+    fn from(value: GetScryfallDataError) -> Self {
+        Self::GetScryfallData(value.into())
+    }
 }
