1780f9ba7fe76729883c0b4133cecce133f36ce1
scadoshi <scottyfermo@hotmail.com>
2026-01-02 15:29:23 -0800

    Generate .env files for both zerver and zwiper in setup scripts

--- zcripts/denv/fedora/setup.sh
+++ zcripts/denv/fedora/setup.sh
@@ -62,8 +62,8 @@ sudo -u postgres createuser --createdb --no-createrole --no-superuser "$CURRENT_
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS zerver;"
 sudo -u postgres psql -c "CREATE DATABASE zerver OWNER $CURRENT_USER;"
 
-# create .env file
-echo "creating .env file..."
+# create .env files
+echo "creating .env files..."
 cat > zerver/.env << EOF
 # app state
 JWT_SECRET=$(openssl rand -base64 32)
@@ -71,6 +71,14 @@ DATABASE_URL=postgres:///zerver?user=$CURRENT_USER
 BIND_ADDRESS=127.0.0.1:3000
 # cors configuration
 ALLOWED_ORIGINS=http://localhost:3000,http://127.0.0.1:3000
+# rust
+RUST_LOG=info
+RUST_BACKTRACE=0
+EOF
+
+cat > zwiper/.env << EOF
+# app state
+BACKEND_URL=http://127.0.0.1:3000
 # rust
 RUST_LOG=info
 RUST_BACKTRACE=0
