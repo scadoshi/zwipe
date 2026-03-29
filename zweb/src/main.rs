62d3b0ca7d9b2328c9d4041b053cfde3245a5bdf
scadoshi <scottyfermo@hotmail.com>
2026-03-29 15:55:59 -0700

    Add PWA icons and apple-touch-icon to zweb
    Wire icon-180, icon-192, icon-512 and site.webmanifest into the
    document head for home screen bookmarks and PWA support.

--- zweb/src/main.rs
+++ zweb/src/main.rs
@@ -8,6 +8,8 @@ pub const API_BASE: &str = "https://api.zwipe.net";
 
 const STYLE: Asset = asset!("/assets/style.css");
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+const APPLE_TOUCH_ICON: Asset = asset!("/assets/icon-180.png");
+const MANIFEST: Asset = asset!("/assets/site.webmanifest");
 const Z_LOGO: &str = include_str!("../assets/z.txt");
 
 #[derive(Routable, Clone, PartialEq)]
@@ -36,6 +38,8 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "apple-touch-icon", href: APPLE_TOUCH_ICON }
+        document::Link { rel: "manifest", href: MANIFEST }
         document::Stylesheet { href: STYLE }
         Router::<Route> {}
     }
