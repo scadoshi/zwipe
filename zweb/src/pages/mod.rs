49b425f419dd14eb466c098ffe9bf7fa4f608a0a
scadoshi <scottyfermo@hotmail.com>
2026-04-01 11:58:35 -0700

    Add discord page, mobile-friendly nav, sized favicon links, Z logo hover effect

--- zweb/src/pages/mod.rs
+++ zweb/src/pages/mod.rs
@@ -1,6 +1,7 @@
 mod about;
 mod android;
 mod contribute;
+mod discord;
 mod home;
 mod ios;
 mod privacy;
@@ -10,6 +11,7 @@ mod verify;
 pub use about::About;
 pub use android::Android;
 pub use contribute::Contribute;
+pub use discord::Discord;
 pub use home::Home;
 pub use ios::Ios;
 pub use privacy::Privacy;
