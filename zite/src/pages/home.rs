1ec92b1b889edcf00a210dc5ffc4e83a70392d5a
scadoshi <scottyfermo@hotmail.com>
2026-08-04 22:20:27 -0700

    content(zite): home banner points at 1.7.5

--- zite/src/pages/home.rs
+++ zite/src/pages/home.rs
@@ -140,7 +140,7 @@ pub fn Home() -> Element {
                 category: "Release",
                 status: BannerStatus::Done,
                 status_label: "New",
-                "Version 1.7.4 just shipped. "
+                "Version 1.7.5 just shipped. "
                 Link { to: Route::Changelog {}, "See what's new" }
             }
         }
